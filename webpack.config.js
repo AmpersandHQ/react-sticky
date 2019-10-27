7d351b87a6fbc8781e271a09ca3f79fc6233865e
Lyndsey Herbert <lhe@amp.co>
2019-10-27 15:37:57 +0000

    ADDED linting

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,44 @@
-const path = require("path");
-const webpack = require("webpack");
+/* eslint-disable */
+/* global require process module __dirname */
+const path = require('path');
+const webpack = require('webpack');
 
-const isLive = process.env.NODE_ENV === "production";
+const isLive = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  mode: isLive ? "production" : "development",
-  devtool: isLive ? "source-map" : "cheap-eval-source-map",
-  entry: {
-    demos: path.resolve("examples", "index.js")
-  },
-  output: {
-    path: path.join(__dirname, "examples"),
-    filename: "bundle.js"
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: "babel-loader"
-      }
-    ]
-  },
-  devServer: {
-    contentBase: path.join(__dirname, "examples"),
-    publicPath: "/",
-    compress: true,
-    port: 9000,
-    historyApiFallback: true
-  }
+    mode: isLive ? 'production' : 'development',
+    devtool: isLive ? 'source-map' : 'cheap-eval-source-map',
+    entry: {
+        demos: path.resolve('examples', 'index.js'),
+    },
+    output: {
+        path: path.join(__dirname, 'examples'),
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+            },
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'eslint-loader',
+                    options: {
+                        enforce: 'pre',
+                    },
+                },
+            },
+        ],
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'examples'),
+        publicPath: '/',
+        compress: true,
+        port: 9000,
+        historyApiFallback: true,
+    },
 };
